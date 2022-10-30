524cdd128e9b5af1ed940e904ef491fb3ac0413c
Miguel Fernandes <miguelffernandes13@gmail.com>
2022-10-30 16:31:11 +0000

    GameDomain

---
+++ Rasbet/Domain/Game.cs
@@ -0,0 +1,12 @@
+﻿using Domain.ResultDomain;
+
+namespace Domain;
+
+public class Game
+{
+    public int Id { get; set; }
+    public DateTime StartTime { get; set; }
+    public virtual Sport Sport { get; set; }
+    public GameState State { get; set; }
+    public virtual ICollection<Result> Results { get; set; } = new List<Result>();
+}
