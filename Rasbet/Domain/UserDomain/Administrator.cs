d770874db5657d5c3fb94675740a561e905b5649
Miguel Fernandes <miguelffernandes13@gmail.com>
2022-10-30 16:31:24 +0000

    Admin and Specialist Domain

---
+++ Rasbet/Domain/UserDomain/Administrator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UserDomain;
+
+public class Administrator : User
+{
+    public Administrator(string name, string email) : base(name, email)
+    {
+    }
+}
