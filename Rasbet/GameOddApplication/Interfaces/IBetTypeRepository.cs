ff956250396457b51466310c660747e72557d07b
Miguel Fernandes <miguelffernandes13@gmail.com>
2022-10-31 11:29:57 +0000

    BetTypeRepository

---
+++ Rasbet/GameOddApplication/Interfaces/IBetTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Domain.ResultDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOddApplication.Interfaces;
+
+public interface IBetTypeRepository
+{
+    public Task<BetType> CreateH2h(double oddHomeTeam, double oddDraw, double oddAwayTeam, Game game);
+
+    public Task<BetType> CreateIndividualResult(Dictionary<string, double> results, Game game);
+}
---
+++ Rasbet/GameOddApplication/Repositories/BetTypeRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Domain.ResultDomain;
+using GameOddApplication.Interfaces;
+using GameOddPersistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOddApplication.Repositories;
+
+public class BetTypeRepository : IBetTypeRepository
+{
+    private readonly GameOddContext gameOddContext;
+
+    public BetTypeRepository(GameOddContext gameOddContext)
+    {
+        this.gameOddContext = gameOddContext;
+    }
+
+    public Task<BetType> CreateH2h(double oddHomeTeam, double oddDraw, double oddAwayTeam, Game g)
+    {
+        BetType betType = new H2h(oddHomeTeam, oddDraw, oddAwayTeam, g);
+        throw new NotImplementedException();
+    }
+
+    public Task<BetType> CreateIndividualResult(Dictionary<string, double> results, Game g)
+    {
+        BetType betType = new IndividualResult(results, g);
+        throw new NotImplementedException();
+    }
+}
