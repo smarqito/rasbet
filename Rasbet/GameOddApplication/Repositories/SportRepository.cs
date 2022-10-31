541f9656a3ec9db82334627cf3aa50da6c178db7
Miguel Fernandes <miguelffernandes13@gmail.com>
2022-10-31 11:30:18 +0000

    SportRepository

---
+++ Rasbet/GameOddApplication/Repositories/SportRepository.cs
@@ -0,0 +1,12 @@
+﻿using GameOddApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOddApplication.Repositories;
+
+public class SportRepository : ISportRepository
+{
+}
