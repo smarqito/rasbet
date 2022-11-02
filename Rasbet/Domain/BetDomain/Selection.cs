b19b012c75eac6052d13148ffce6530e6a8862b9
José Malheiro <jafmalheiro@live.com.pt>
2022-11-02 00:54:38 +0000

    refactor: Selection domain

--- Rasbet/Domain/BetDomain/Selection.cs
+++ Rasbet/Domain/BetDomain/Selection.cs
@@ -4,6 +4,7 @@ namespace Domain;
 
 public class Selection
 {
+    public int id { get; set; }
     public int OddId { get; set; }
     public double Odd { get; set; }
     public virtual BetType Result { get; set; }
