c417dc459b43adbbb75c4e5de0a1b866593ab5ee
José Malheiro <jafmalheiro@live.com.pt>
2022-11-03 17:16:04 +0000

    facade started

--- Rasbet/BetFacade/BetFacade.cs
+++ Rasbet/BetFacade/BetFacade.cs
@@ -1,7 +1,7 @@
 ﻿using BetApplication.Errors;
 using BetApplication.Repositories;
 using Domain;
-using Domain.ResultDomain;
+using DTO;
 using GameOddApplication.Repositories;
 using UserApplication.Repositories;
 
@@ -46,17 +46,11 @@ public class BetFacade : IBetFacade
         catch(UserBalanceTooLowException e) {
             //await BetRepository.DeleteBet(bet.Id);
             throw new Exception(e.Message);
-
         }
-        catch(BetTooLowException e)
+        catch(Exception e)
         {
             throw new Exception(e.Message);
         }
-        catch(SelectionNotExistException e)
-        {
-            throw new Exception(e.Message);
-        }
-
     }
 
     public async Task<BetMultiple> CreateBetMultiple(double amount, DateTime start, int userId, double oddMultiple, ICollection<int> selectionIds)
@@ -69,7 +63,7 @@ public class BetFacade : IBetFacade
                 var selection = await SelectionRepository.GetSelectionById(selectionId);
                 selections.Add(selection);
             }
-            catch(SelectionNotExistException e)
+            catch(Exception e)
             {
                 throw new Exception(e.Message);
             }
@@ -82,13 +76,12 @@ public class BetFacade : IBetFacade
 
             return bet;
         }
-        catch (UserBalanceTooLowException e)
+        catch(UserBalanceTooLowException e)
         {
             //await BetRepository.DeleteBet(bet.Id);
             throw new Exception(e.Message);
-
         }
-        catch (BetTooLowException e)
+        catch (Exception e)
         {
             throw new Exception(e.Message);
         }
@@ -96,41 +89,113 @@ public class BetFacade : IBetFacade
 
     public async Task<ICollection<Bet>> GetUserBetsByState(int user, BetState state)
     {
-        return await BetRepository.GetUserBetsByState(user, state);
+        try
+        {
+            return await BetRepository.GetUserBetsByState(user, state);
+        }
+        catch(Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+    public async Task<ICollection<Bet>> GetUserBetsByStart(int user, DateTime start)
+    {
+        try
+        {
+            return await GetUserBetsByStart(user, start);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message); 
+        }
     }
 
-    public async Task<bool> UpdateBets(ICollection<int> bets, BetState state)
+    public async Task<ICollection<Bet>> GetUserBetsByAmount(int user, double amount)
     {
-        return await BetRepository.UpdateBets(bets, state);
+        try
+        {
+            return await GetUserBetsByAmount(user, amount);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<ICollection<Bet>> GetUserBetsByEnd(int user, DateTime end)
+    {
+        try
+        {
+            return await GetUserBetsByEnd(user, end);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<ICollection<Bet>> GetUserBetsByWonValue(int user, double wonValue)
+    {
+        try
+        {
+            return await GetUserBetsByWonValue(user, wonValue);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<bool> UpdateBets(ICollection<BetsOddsWonDTO> finishedGames)
+    {
+        try
+        {
+            return await BetRepository.UpdateBets(finishedGames);
+        }
+        catch(Exception e)
+        {
+            throw new Exception(e.Message);
+        }
     }
 
     // Métodos para o SelectionRepository
-    public async Task<Selection> CreateSelection(int betTypeId, int oddId)
+    public async Task<Selection> CreateSelection(int betTypeId, int oddId, double odd)
     {
-        Odd oddChosen;
+        // pedir GameOddApi com betTypeId + oddId
+        // receber odd atual do servidor
+        // comparar com odd do cliente e aplicar um threshold max
+        //      caso threshold exceda, enviar erro
+        //      caso contrario, criar selection
+
+        Odd serverOdd;
         // get odd by id 
-        BetType bettype;
 
         try
         {
-            //return await SelectionRepository.CreateSelection(bettype, oddChosen);
+            //return await SelectionRepository.CreateSelection(serverOdd, odd, betTypeId, bettype.Game.Id);
             throw new NotImplementedException();
         }
         catch(Exception e)
         {
             throw new Exception(e.Message);
         }
-
-
     }
 
     public async Task<ICollection<Selection>> GetSelectionByGame(int game)
     {
-        return await SelectionRepository.GetSelectionByGame(game);
+        try
+        {
+            return await SelectionRepository.GetSelectionByGame(game);
+        }
+        catch(Exception e)
+        {
+            throw new Exception(e.Message);
+        }
     }
 
     public async Task<ICollection<Selection>> GetSelectionByType(int bettype)
     {
         return await SelectionRepository.GetSelectionByType(bettype);
     }
+
 }
--- Rasbet/BetFacade/IBetFacade.cs
+++ Rasbet/BetFacade/IBetFacade.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using DTO;
 
 namespace BetFacade;
 
@@ -7,8 +8,12 @@ public interface IBetFacade
     Task<BetSimple> CreateBetSimple(double amount, DateTime start, int userId, int selectionId);
     Task<BetMultiple> CreateBetMultiple(double amount, DateTime start, int userId, double oddMultiple, ICollection<int> selectionIds);
     Task<ICollection<Bet>> GetUserBetsByState(int user, BetState state);
-    Task<bool> UpdateBets(ICollection<int> bets, BetState state);
-    Task<Selection> CreateSelection(int betTypeId, int oddId);
+    Task<ICollection<Bet>> GetUserBetsByStart(int user, DateTime start);
+    Task<ICollection<Bet>> GetUserBetsByAmount(int user, double amount);
+    Task<ICollection<Bet>> GetUserBetsByEnd(int user, DateTime end);
+    Task<ICollection<Bet>> GetUserBetsByWonValue(int user, double wonValue);
+    Task<bool> UpdateBets(ICollection<BetsOddsWonDTO> finsihedGames);
+    Task<Selection> CreateSelection(int betTypeId, int oddId, double odd);
     Task<ICollection<Selection>> GetSelectionByGame(int game);
     Task<ICollection<Selection>> GetSelectionByType(int bettype);
 }
